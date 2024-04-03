00e2228de03c08fbc7e33f5b4f58014ca1273b2b
George Omara <georgeomara8@gmail.com>
2024-04-03 20:37:12 +0300

    Custom fonts setup and configurations

---
+++ src/app/fonts.ts
@@ -0,0 +1,35 @@
+import localFont from "next/font/local";
+
+export const trajanPro = localFont({
+  src: [
+    {
+      path: "../fonts-local/TrajanPro-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../fonts-local/TrajanPro-Bold.otf",
+      weight: "700",
+      style: "bold",
+    },
+  ],
+});
+
+export const proximaNova = localFont({
+  src: [
+    {
+      path: "../fonts-local/Proxima-Nova-Light.otf",
+      weight: "100",
+      style: "light",
+    },
+    {
+      path: "../fonts-local/Proxima-Nova-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+});
+
+export const delius = localFont({ src: "../fonts-local/Delius-Regular.ttf" });
+
+export const megabyte = localFont({ src: "../fonts-local/MegabyteLight.otf" });
