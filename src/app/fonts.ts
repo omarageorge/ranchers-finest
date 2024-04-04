7555dba81aa90fed32c2e06a18d3c89b8478020c
George Omara <georgeomara8@gmail.com>
2024-04-04 19:10:33 +0300

    Add font to local font file

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,16 +1,16 @@
-import localFont from "next/font/local";
+import localFont from 'next/font/local';
 
 export const trajanPro = localFont({
   src: [
     {
-      path: "../fonts-local/TrajanPro-Regular.ttf",
-      weight: "400",
-      style: "normal",
+      path: '../fonts-local/TrajanPro-Regular.ttf',
+      weight: '400',
+      style: 'normal',
     },
     {
-      path: "../fonts-local/TrajanPro-Bold.otf",
-      weight: "700",
-      style: "bold",
+      path: '../fonts-local/TrajanPro-Bold.otf',
+      weight: '700',
+      style: 'bold',
     },
   ],
 });
@@ -18,18 +18,23 @@ export const trajanPro = localFont({
 export const proximaNova = localFont({
   src: [
     {
-      path: "../fonts-local/Proxima-Nova-Light.otf",
-      weight: "100",
-      style: "light",
+      path: '../fonts-local/Proxima-Nova-Light.otf',
+      weight: '100',
+      style: 'light',
     },
     {
-      path: "../fonts-local/Proxima-Nova-Regular.otf",
-      weight: "400",
-      style: "normal",
+      path: '../fonts-local/Proxima-Nova-Regular.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../fonts-local/Proxima-Nova-Semibold.otf',
+      weight: '600',
+      style: 'semibold',
     },
   ],
 });
 
-export const delius = localFont({ src: "../fonts-local/Delius-Regular.ttf" });
+export const delius = localFont({ src: '../fonts-local/Delius-Regular.ttf' });
 
-export const megabyte = localFont({ src: "../fonts-local/MegabyteLight.otf" });
+export const megabyte = localFont({ src: '../fonts-local/MegabyteLight.otf' });
