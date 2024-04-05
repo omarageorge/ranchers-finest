65a65daa80f52da0d43a679895a2bfbf956b50ab
George Omara <georgeomara8@gmail.com>
2024-04-05 16:02:53 +0300

    Introduce nested container within row for layout control

--- src/components/ui/footer/row.tsx
+++ src/components/ui/footer/row.tsx
@@ -1,8 +1,10 @@
 const Row: React.FC<{
   children: React.ReactElement<HTMLDivElement>[];
 }> = ({ children }) => (
-  <div className='flex flex-col items-start justify-between gap-14 py-20 px-8 md:flex-row md:gap-0 lg:px-0 lg:py-28'>
-    {children}
+  <div className='w-full py-20 px-8 lg:px-10 xl:px-0 lg:py-28'>
+    <div className='flex flex-col items-start justify-between gap-14 md:flex-row md:gap-0'>
+      {children}
+    </div>
   </div>
 );
 
