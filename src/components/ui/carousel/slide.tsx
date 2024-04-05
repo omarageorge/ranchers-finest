b8d03a19ee32d5f6204423299ce7df9f575ce6cc
George Omara <georgeomara8@gmail.com>
2024-04-05 13:59:18 +0300

    Add scale effect and image priority for active Slide

--- src/components/ui/carousel/slide.tsx
+++ src/components/ui/carousel/slide.tsx
@@ -10,12 +10,13 @@ const Slide: React.FC<{
     <div
       className={clsx(
         'w-full h-full absolute opacity-0 transition-all duration-1000 ease-in-out',
-        isActive && 'opacity-100 duration-100'
+        isActive && 'opacity-100 duration-1000 transform scale-105'
       )}
     >
       <Image
         src={src}
         alt={alt}
+        priority
         className='w-full h-full object-cover object-center '
       />
     </div>
