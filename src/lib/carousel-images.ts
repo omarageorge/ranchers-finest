5d0079776316da37981c8b10c640842526ca9c96
George Omara <georgeomara8@gmail.com>
2024-04-05 15:02:02 +0300

    Fixed wrong imports

--- src/lib/carousel-images.ts
+++ src/lib/carousel-images.ts
@@ -1,7 +1,9 @@
 import image1 from '@/assets/carousel/01.jpg';
 import image2 from '@/assets/carousel/02.jpg';
 import image3 from '@/assets/carousel/03.jpg';
-import { default as image4, default as image5 } from '@/assets/carousel/05.jpg';
+import image4 from '@/assets/carousel/04.jpg';
+import image5 from '@/assets/carousel/05.jpg';
+
 import { StaticImageData } from 'next/image';
 
 const images: StaticImageData[] = [image1, image2, image3, image4, image5];
