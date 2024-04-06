e9d95fb1ff7e551ba653b7bc5942026cd3892dc1
George Omara <georgeomara8@gmail.com>
2024-04-06 20:08:57 +0300

    Clean up code, remove unnecessary code

--- src/components/ui/footer/row.tsx
+++ src/components/ui/footer/row.tsx
@@ -1,10 +1,10 @@
-const Row: React.FC<{
+interface RowProp {
   children: React.ReactElement<HTMLDivElement>[];
-}> = ({ children }) => (
-  <div className='w-full py-20 px-8 lg:px-10 xl:px-0 lg:py-28 xl:py-32'>
-    <div className='flex flex-col items-start justify-between gap-14 md:flex-row md:gap-0'>
-      {children}
-    </div>
+}
+
+const Row: React.FC<RowProp> = ({ children }) => (
+  <div className='flex flex-col items-start justify-between gap-14 md:flex-row md:gap-0'>
+    {children}
   </div>
 );
 
