9c060690985466e637c8855b87b941a6e3b440ef
George Omara <georgeomara8@gmail.com>
2024-04-06 15:52:45 +0300

    Update h3 to p

--- src/components/ui/customer-review-card.tsx
+++ src/components/ui/customer-review-card.tsx
@@ -7,7 +7,7 @@ const CustomerReviewCard: React.FC<CustomerReviewCardProp> = ({
   customer,
 }) => (
   <article className='w-full h-auto flex flex-col items-center justify-center gap-5 bg-whitish rounded-md px-4 py-6 xl:h-56'>
-    <h3 className='font-light text-lg text-center lg:text-xl'>{message}</h3>
+    <p className='font-light text-lg text-center lg:text-xl'>{message}</p>
     <p className='font-bold text-xl text-center'>{customer}</p>
   </article>
 );
