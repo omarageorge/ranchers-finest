aa0f984ecfc2314af8c95509cd24622215e477b4
George Omara <georgeomara8@gmail.com>
2024-04-06 14:07:57 +0300

    Adjust vertical padding to 8rem for extra large screens

--- src/components/ui/footer/row.tsx
+++ src/components/ui/footer/row.tsx
@@ -1,7 +1,7 @@
 const Row: React.FC<{
   children: React.ReactElement<HTMLDivElement>[];
 }> = ({ children }) => (
-  <div className='w-full py-20 px-8 lg:px-10 xl:px-0 lg:py-28'>
+  <div className='w-full py-20 px-8 lg:px-10 xl:px-0 lg:py-28 xl:py-32'>
     <div className='flex flex-col items-start justify-between gap-14 md:flex-row md:gap-0'>
       {children}
     </div>
