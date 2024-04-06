8831aa78732a3c3f2e1c43bf3c42279e498e6b91
George Omara <georgeomara8@gmail.com>
2024-04-06 14:22:05 +0300

    Add customer review section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { delius, trajanPro } from '@/app/fonts';
 import Carousel from '@/components/ui/carousel/carousel';
-import React from 'react';
+import CustomerReviewCard from '@/components/ui/customer-review-card';
+import { customerReviews } from '@/lib/data';
+import { Fragment } from 'react';
 
 export default function Home() {
   return (
-    <React.Fragment>
+    <Fragment>
       <header className='w-full h-[calc(100vh-5rem)] relative z-0 md:h-[calc(100vh-6rem)] lg:h-[calc(100vh-8rem)]'>
         <Carousel />
         <div className='w-full h-full absolute z-20 bg-[#000000] opacity-65'></div>
@@ -21,6 +23,20 @@ export default function Home() {
           </h2>
         </div>
       </header>
-    </React.Fragment>
+      <section className='w-full h-auto bg-[url("/ranch-bg.jpg")] bg-cover bg-center px-8 py-20 md:py-28 xl:px-0 xl:py-32'>
+        <div className='space-y-10 xl:container xl:mx-auto xl:space-y-14'>
+          <h3
+            className={`${trajanPro.className} font-bold text-white text-2xl text-center md:text-4xl`}
+          >
+            WHAT OUR CUSTOMERS SAY
+          </h3>
+          <div className='grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3'>
+            {customerReviews.map((review, index) => (
+              <CustomerReviewCard key={index} {...review} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </Fragment>
   );
 }
