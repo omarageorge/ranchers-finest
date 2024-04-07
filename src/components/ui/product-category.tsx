bcc65d8442754e363df2f4e981660796bf296085
George Omara <georgeomara8@gmail.com>
2024-04-07 11:20:09 +0300

    Create product category reusable component

---
+++ src/components/ui/product-category.tsx
@@ -0,0 +1,33 @@
+import Image, { StaticImageData } from 'next/image';
+import Link from 'next/link';
+
+interface ProductCategoryProps {
+  icon: StaticImageData;
+  name: string;
+  description: string;
+  url: string;
+}
+
+const ProductCategory: React.FC<ProductCategoryProps> = ({
+  icon,
+  name,
+  description,
+  url,
+}) => {
+  return (
+    <div className='grid grid-cols-1 gap-6'>
+      <Image src={icon} alt={name} className='w-24 mx-auto lg:w-34 h-auto' />
+      <div className='text-center'>
+        <div className='h-44 space-y-3'>
+          <h4 className='font-normal text-2xl text-white uppercase'>{name}</h4>
+          <p className='p1 text-whitish'>{description}</p>
+        </div>
+        <Link href={url} className='font-semibold text-xl text-reddish'>
+          SEE ALL
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCategory;
