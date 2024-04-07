2a3d0ca3f233c26bcf7208eadb7069dc85636001
George Omara <georgeomara8@gmail.com>
2024-04-07 11:19:23 +0300

    Create product category type definition

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -1,3 +1,5 @@
+import { StaticImageData } from 'next/image';
+
 export type SocialMedia = {
   url: string;
   icon: React.ComponentType;
@@ -6,4 +8,11 @@ export type SocialMedia = {
 export type CustomerReview = {
   message: string;
   customer: string;
+};
+
+export type ProductCategory = {
+  icon: StaticImageData;
+  name: string;
+  description: string;
+  url: string;
 };
