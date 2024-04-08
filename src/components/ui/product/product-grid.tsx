99aad60b9bbe580b75969b74457e096961607033
George Omara <georgeomara8@gmail.com>
2024-04-08 10:20:13 +0300

    Include optional padding property

--- src/components/ui/product/product-grid.tsx
+++ src/components/ui/product/product-grid.tsx
@@ -7,22 +7,15 @@ import { Fragment } from 'react';
 const ProductGrid: React.FC<{
   products: Product[];
   title?: string;
-  top?: boolean;
-}> = ({ products, title, top }) => {
+}> = ({ products, title }) => {
   return (
     <Fragment>
-      <div className='bg-red'>
-        {title && (
-          <h3
-            className={clsx(
-              `${trajanPro.className} text-center text-raven`,
-              top && 'py-28'
-            )}
-          >
-            {title}
-          </h3>
-        )}
-      </div>
+      {title && (
+        <h3 className={`${trajanPro.className} text-center text-raven py-20`}>
+          {title}
+        </h3>
+      )}
+
       <div className='w-full grid grid-cols-1 gap-y-20 justify-center md:grid-cols-2 lg:grid-cols-3'>
         {products.map((product) => (
           <ProductCard key={product.name} {...product} />
@@ -34,10 +27,17 @@ const ProductGrid: React.FC<{
 
 export default ProductGrid;
 
-export const GridContainer: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => (
-  <main className='w-full bg-white px-8 py-20 md:px-10 md:py-28 xl:px-20 xl:py-32 xl:container xl:mx-auto'>
+export const GridContainer: React.FC<{
+  children: React.ReactNode;
+  padding?: boolean;
+}> = ({ children, padding }) => (
+  // <main className='w-full bg-white px-8 py-20 md:px-10 md:py-28 xl:px-20 xl:py-32 xl:container xl:mx-auto'>
+  <main
+    className={clsx(
+      'w-full bg-white px-8 pb-20 md:px-10 md:pb-28 xl:px-20 xl:pb-32 xl:container xl:mx-auto',
+      padding && 'pt-20 md:pt-28 xl:pt-32'
+    )}
+  >
     {children}
   </main>
 );
