51729ce26e7068c4b96025573af6106dfcfd9458
George Omara <georgeomara8@gmail.com>
2024-04-08 15:33:09 +0300

    Increase overflow opacity to 60

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     <Fragment>
       <header className='w-full h-[calc(100vh-5rem)] relative z-0 md:h-[calc(100vh-6rem)] lg:h-[calc(100vh-8rem)]'>
         <Carousel />
-        <div className='w-full h-full absolute z-20 bg-[#000000] opacity-55'></div>
+        <div className='w-full h-full absolute z-20 bg-[#000000] opacity-60'></div>
         <div className='w-full h-full absolute z-30 flex flex-col items-center justify-center text-center gap-4 md:gap-7 '>
           <h1
             className={`${trajanPro.className} text-white font-bold text-5xl lg:text-7xl xl:text-8xl`}
