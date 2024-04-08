9de6a932e4d194223479e4149e04eb07569ca671
George Omara <georgeomara8@gmail.com>
2024-04-09 00:37:35 +0300

    Add contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,45 @@
+import contact from '@/assets/hero/hero-contact.jpg';
+import HeroSection from '@/components/ui/hero-section';
+import { Fragment } from 'react';
+import { trajanPro } from '../fonts';
+
 function Contact() {
   return (
-    <main className='w-full h-auto'>
-      <div className='w-full px-8 py-20 md:px-10 md:py-28 xl:px-20 xl:py-36 xl:container xl:mx-auto'></div>
-    </main>
+    <Fragment>
+      <HeroSection title='Contact Us' image={contact} />
+      <main className='w-full h-auto'>
+        <div className='w-full px-8 py-20 md:px-10 md:py-28 xl:px-20  xl:container xl:mx-auto'>
+          <div className='w-full lg:w-10/12 xl:w-8/12 mx-auto'>
+            <form className='space-y-4'>
+              <div className='grid grid-cols-1 gap-x-4 gap-y-4 md:grid-cols-2 md:gap-y-0'>
+                <input type='text' placeholder='First Name' />
+                <input type='text' placeholder='Last Name' />
+              </div>
+              <div className='form-group'>
+                <input type='email' placeholder='Email' />
+              </div>
+              <div className='form-group'>
+                <input type='text' placeholder='Subject' />
+              </div>
+              <div className='form-group'>
+                <input type='tel' placeholder='Phone' />
+              </div>
+              <div className='form-group'>
+                <textarea placeholder='Message'></textarea>
+              </div>
+              <div className=''>
+                <button
+                  type='submit'
+                  className={`${trajanPro.className} bg-black hover:bg-red text-white px-8 py-4 rounded-sm transition-all duration-300 ease-in`}
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </main>
+    </Fragment>
   );
 }
 
