e07ffaf8058cd0ad1a6d928c4cc1d43a95b64cb1
George Omara <georgeomara8@gmail.com>
2024-04-08 10:24:34 +0300

    Change overlay color to black raven

--- src/components/ui/hero-section.tsx
+++ src/components/ui/hero-section.tsx
@@ -14,7 +14,7 @@ const HeroSection: React.FC<HeroSectionProps> = ({ title, image }) => {
         priority
         className='block w-full h-full absolute z-10 object-cover object-center'
       />
-      <div className='w-full h-full absolute z-20 bg-reddish opacity-80'></div>
+      <div className='w-full h-full absolute z-20 bg-raven opacity-80'></div>
       <div className='w-full h-full absolute z-30 flex items-center justify-center'>
         <h1
           className={`${trajanPro.className} font-bold text-4xl text-center text-white md:text-6xl`}
