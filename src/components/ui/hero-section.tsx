5b31b3bdadfcec881375dae0fe18059c176d90d4
George Omara <georgeomara8@gmail.com>
2024-04-09 10:04:45 +0300

    Reduce height for medium screens

--- src/components/ui/hero-section.tsx
+++ src/components/ui/hero-section.tsx
@@ -7,10 +7,11 @@ interface HeroSectionProps {
 }
 const HeroSection: React.FC<HeroSectionProps> = ({ title, image }) => {
   return (
-    <div className='w-full h-[calc(100vh-25vh)] relative z-0 md:h-[calc(100vh-55vh)] xl:h-[calc(100vh-25vh)]'>
+    <div className='w-full h-[calc(100vh-40vh)] bg-red relative z-0 md:h-[calc(100vh-40vh)] xl:h-[calc(100vh-25vh)]'>
       <Image
         src={image}
         alt={title}
+        placeholder='blur'
         priority
         className='block w-full h-full absolute z-10 object-cover object-center'
       />
