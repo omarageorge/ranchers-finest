5dacd2fb3aedb220c2af0cb70696c53935965ed9
George Omara <georgeomara8@gmail.com>
2024-04-13 15:24:22 +0300

    Add products to product list

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -1,6 +1,8 @@
+import Betar_Mini_Beefyz_Sausages from '@/assets/products/sausages/Betar_Mini_Beefyz_Sausages.png';
 import Ranchers_Beef_Sausages_800g from '@/assets/products/sausages/Ranchers_Beef_Sausages_800g.png';
 import Ranchers_Chicken_Sausages_400g from '@/assets/products/sausages/Ranchers_Chicken_Sausages_400g.png';
 import Ranchers_Chicken_Sausages_800g from '@/assets/products/sausages/Ranchers_Chicken_Sausages_800g.png';
+import Ranchers_English_Beef_Sausages_500g from '@/assets/products/sausages/Ranchers_English_Beef_Sausages_500g.png';
 import Ranchers_Goat_Sausages_400g from '@/assets/products/sausages/Ranchers_Goat_Sausages_400g.png';
 import Ranchers_Goat_Sausages_800g from '@/assets/products/sausages/Ranchers_Goat_Sausages_800g.png';
 import Ranchers_Pork_Hotdogs_350g from '@/assets/products/sausages/Ranchers_Pork_Hotdogs_350g.png';
@@ -8,29 +10,185 @@ import Ranchers_Pork_Sausages_400g from '@/assets/products/sausages/Ranchers_Por
 import Ranchers_Pork_Sausages_800g from '@/assets/products/sausages/Ranchers_Pork_Sausages_800g.png';
 import Ranchers_Smokies_Beef_400g from '@/assets/products/sausages/Ranchers_Smokies_Beef_400g.png';
 
-import Ranchers_Beef_Mortadella_200g from '@/assets/products/sausages/Ranchers_Beef_Mortadella_200g.png';
+import Ranchers_Beef_BBQ_Sausages_1kg from '@/assets/products/sausages/Ranchers_Beef_BBQ_Sausages_1kg.png';
+import Ranchers_Beef_BBQ_Sausages_500g from '@/assets/products/sausages/Ranchers_Beef_BBQ_Sausages_500g.png';
+import Ranchers_Beef_Hot_Dogs_1kg from '@/assets/products/sausages/Ranchers_Beef_Hot_Dogs_1kg.png';
+import Ranchers_Breakfast_Beef_350g from '@/assets/products/sausages/Ranchers_Breakfast_Beef_350g.png';
+import Ranchers_English_Beef_Sausages_1kg from '@/assets/products/sausages/Ranchers_English_Beef_Sausages_1kg.png';
+import Ranchers_Traditional_Smoked_Beef_Sausages from '@/assets/products/sausages/Ranchers_Traditional_Smoked_Beef_Sausages.png';
+
+import Ranchers_Breakfast_Chicken_Sausages_350g from '@/assets/products/sausages/Ranchers_Breakfast_Chicken_Sausages_350g.png';
+import Ranchers_Breakfast_Chicken_Sausages_700g from '@/assets/products/sausages/Ranchers_Breakfast_Chicken_Sausages_700g.png';
+import Ranchers_English_Chicken_Sausages_1kg from '@/assets/products/sausages/Ranchers_English_Chicken_Sausages_1kg.png';
+
+import Ranchers_BBQ_Pork_Sausages_1kg from '@/assets/products/sausages/Ranchers_BBQ_Pork_Sausages_1kg.png';
+import Ranchers_Breakfast_Pork_Sausages_700g from '@/assets/products/sausages/Ranchers_Breakfast_Pork_Sausages_700g.png';
+import Ranchers_English_Pork_Sausages_1kg from '@/assets/products/sausages/Ranchers_English_Pork_Sausages_1kg.png';
+import Ranchers_English_Pork_Sausages_500g from '@/assets/products/sausages/Ranchers_English_Pork_Sausages_500g.png';
+import Ranchers_Pork_Hot_Dogs_1kg from '@/assets/products/sausages/Ranchers_Pork_Hot_Dogs_1kg.png';
+
+import Betar_Beef_Burgers_4pcs from '@/assets/products/frozen_cuts/Betar_Beef_Burgers_4pcs.png';
+import Ranchers_Beef_Burgers_4pcs from '@/assets/products/frozen_cuts/Ranchers_Beef_Burgers_4pcs.png';
+import Ranchers_Pork_Chops_4pcs from '@/assets/products/frozen_cuts/Ranchers_Pork_Chops_4pcs.png';
+import Ranchers_Pork_Cubes_1kg from '@/assets/products/frozen_cuts/Ranchers_Pork_Cubes_1kg.png';
+import Ranchers_Pork_Cubes_500g from '@/assets/products/frozen_cuts/Ranchers_Pork_Cubes_500g.png';
+import Ranchers_Prime_Beef_Ribeye_200g from '@/assets/products/frozen_cuts/Ranchers_Prime_Beef_Ribeye_200g.png';
+import Ranchers_Prime_Beef_Rump_Steak_200g from '@/assets/products/frozen_cuts/Ranchers_Prime_Beef_Rump_Steak_200g.png';
+import Ranchers_Prime_Beef_Sirloin_Steak_200g from '@/assets/products/frozen_cuts/Ranchers_Prime_Beef_Sirloin_Steak_200g.png';
+import Ranchers_Prime_Beef_Steak_200g from '@/assets/products/frozen_cuts/Ranchers_Prime_Beef_Steak_200g.png';
+
+import Ranchers_Beef_Burger_Patties_4pcs from '@/assets/products/frozen_cuts/Ranchers_Beef_Burger_Patties_4pcs.png';
+import Ranchers_Beef_Cubes_500g from '@/assets/products/frozen_cuts/Ranchers_Beef_Cubes_500g.png';
+import Ranchers_Chicken_Drumsticks_1kg from '@/assets/products/frozen_cuts/Ranchers_Chicken_Drumsticks_1kg.png';
+import Ranchers_Chicken_Fillet_1kg from '@/assets/products/frozen_cuts/Ranchers_Chicken_Fillet_1kg.png';
+import Ranchers_Chicken_Legs_1kg from '@/assets/products/frozen_cuts/Ranchers_Chicken_Legs_1kg.png';
+import Ranchers_Chicken_Thighs_1kg from '@/assets/products/frozen_cuts/Ranchers_Chicken_Thighs_1kg.png';
+import Ranchers_Chicken_Wings_1kg from '@/assets/products/frozen_cuts/Ranchers_Chicken_Wings_1kg.png';
+
+import Ranchers_Back_Bacon_200g from '@/assets/products/cold_cuts/Ranchers_Back_Bacon_200g.png';
+import Ranchers_Beef_Mortadella_200g from '@/assets/products/cold_cuts/Ranchers_Beef_Mortadella_200g.png';
+import Ranchers_Garlic_Salami_200g from '@/assets/products/cold_cuts/Ranchers_Garlic_Salami_200g.png';
+import Ranchers_Roasted_Turkey_Fillet_200g from '@/assets/products/cold_cuts/Ranchers_Roasted_Turkey_Fillet_200g.png';
+import Ranchers_Sandwich_Ham_200g from '@/assets/products/cold_cuts/Ranchers_Sandwich_Ham_200g.png';
+import Ranchers_Streaky_Bacon_200g from '@/assets/products/cold_cuts/Ranchers_Streaky_Bacon_200g.png';
 
 import { ProductData } from '@/lib/definitions';
+
+export const frozenCuts: ProductData = {
+  products: [
+    {
+      name: 'Prime Beef Ribeye (200g)',
+      image: Ranchers_Prime_Beef_Ribeye_200g,
+    },
+    {
+      name: 'Prime Beef Rump Steak (200g)',
+      image: Ranchers_Prime_Beef_Rump_Steak_200g,
+    },
+    {
+      name: 'Prime Beef Sirloin Steak (200g)',
+      image: Ranchers_Prime_Beef_Sirloin_Steak_200g,
+    },
+    {
+      name: 'Prime Beef Sirloin Steak (200g)',
+      image: Ranchers_Prime_Beef_Sirloin_Steak_200g,
+    },
+    { name: 'Prime Beef Steak (200g)', image: Ranchers_Prime_Beef_Steak_200g },
+    { name: 'Pork_Chops_(4pcs)', image: Ranchers_Pork_Chops_4pcs },
+    { name: 'Pork_Cubes_(1kg)', image: Ranchers_Pork_Cubes_1kg },
+    { name: 'Pork_Cubes_(500g)', image: Ranchers_Pork_Cubes_500g },
+    { name: 'Beef_Burgers_(4pcs)', image: Ranchers_Beef_Burgers_4pcs },
+    { name: 'Beef_Burgers_(4pcs)', image: Betar_Beef_Burgers_4pcs },
+    {
+      name: 'Beef Burger Patties (4pcs)',
+      image: Ranchers_Beef_Burger_Patties_4pcs,
+    },
+    { name: 'Beef Cubes (500g)', image: Ranchers_Beef_Cubes_500g },
+    { name: 'Chicken Legs (1kg)', image: Ranchers_Chicken_Legs_1kg },
+    { name: 'Chicken Thighs (1kg)', image: Ranchers_Chicken_Thighs_1kg },
+    { name: 'Chicken Wings (1kg)', image: Ranchers_Chicken_Wings_1kg },
+    {
+      name: 'Chicken Drumsticks (1kg)',
+      image: Ranchers_Chicken_Drumsticks_1kg,
+    },
+    { name: 'Chicken Fillet (1kg)', image: Ranchers_Chicken_Fillet_1kg },
+  ],
+};
+
+export const goatSausages: ProductData = {
+  products: [
+    { name: 'Goat Sausages (400g)', image: Ranchers_Goat_Sausages_400g },
+    { name: 'Goat Sausages (800g)', image: Ranchers_Goat_Sausages_800g },
+  ],
+};
+
+export const porkSausages: ProductData = {
+  products: [
+    { name: 'Pork Hotdogs (350g)', image: Ranchers_Pork_Hotdogs_350g },
+    { name: 'Pork Sausages (400g)', image: Ranchers_Pork_Sausages_400g },
+    { name: 'Pork Sausages (800g)', image: Ranchers_Pork_Sausages_800g },
+    {
+      name: 'Breakfast Pork Sausages (700g)',
+      image: Ranchers_Breakfast_Pork_Sausages_700g,
+    },
+    {
+      name: 'English Pork Sausages (500g)',
+      image: Ranchers_English_Pork_Sausages_500g,
+    },
+    {
+      name: 'English Pork Sausages (1kg)',
+      image: Ranchers_English_Pork_Sausages_1kg,
+    },
+    { name: 'BBQ Pork Sausages (1kg)', image: Ranchers_BBQ_Pork_Sausages_1kg },
+    { name: 'Pork Hot Dogs (1kg)', image: Ranchers_Pork_Hot_Dogs_1kg },
+  ],
+};
 
 export const beefSausages: ProductData = {
   products: [
     { name: 'Beef Sausages (800g)', image: Ranchers_Beef_Sausages_800g },
+    { name: 'Smokies Beef (400g)', image: Ranchers_Smokies_Beef_400g },
+    {
+      name: 'English Beef Sausages (500g)',
+      image: Ranchers_English_Beef_Sausages_500g,
+    },
+    {
+      name: 'English Beef Sausages (1kg)',
+      image: Ranchers_English_Beef_Sausages_1kg,
+    },
+
+    { name: 'Beef BBQ Sausages (1kg)', image: Ranchers_Beef_BBQ_Sausages_1kg },
+    { name: 'Breakfast Beef (350g)', image: Ranchers_Breakfast_Beef_350g },
+    { name: 'Beef Hot Dogs (1kg)', image: Ranchers_Beef_Hot_Dogs_1kg },
+    {
+      name: 'Beef BBQ Sausages (500g)',
+      image: Ranchers_Beef_BBQ_Sausages_500g,
+    },
+    {
+      name: 'Traditional Smoked Beef Sausages',
+      image: Ranchers_Traditional_Smoked_Beef_Sausages,
+    },
+    { name: 'Betar Mini Beefyz Sausages', image: Betar_Mini_Beefyz_Sausages },
+  ],
+};
+
+export const chickenSausages: ProductData = {
+  products: [
     { name: 'Chicken Sausages (400g)', image: Ranchers_Chicken_Sausages_400g },
     { name: 'Chicken Sausages (800g)', image: Ranchers_Chicken_Sausages_800g },
-    { name: 'Goat Sausages (400g)', image: Ranchers_Goat_Sausages_400g },
-    { name: 'Goat Sausages (800g)', image: Ranchers_Goat_Sausages_800g },
-    { name: 'Pork Hotdogs (350g)', image: Ranchers_Pork_Hotdogs_350g },
-    { name: 'Pork Sausages (400g)', image: Ranchers_Pork_Sausages_400g },
-    { name: 'Pork Sausages (800g)', image: Ranchers_Pork_Sausages_800g },
-    { name: 'Smokies Beef (400g)', image: Ranchers_Smokies_Beef_400g },
+    {
+      name: 'English Chicken Sausages (1kg)',
+      image: Ranchers_English_Chicken_Sausages_1kg,
+    },
+    {
+      name: 'Breakfast Chicken Sausages (350g)',
+      image: Ranchers_Breakfast_Chicken_Sausages_350g,
+    },
+    {
+      name: 'Breakfast Chicken Sausages (700g)',
+      image: Ranchers_Breakfast_Chicken_Sausages_700g,
+    },
   ],
 };
 
 export const coldCuts: ProductData = {
   products: [
     {
-      name: 'Ranchers_Beef_Mortadella_200g',
+      name: 'Ranchers Beef Mortadella (200g)',
       image: Ranchers_Beef_Mortadella_200g,
+    },
+    {
+      name: 'Ranchers Garlic Salami (200g)',
+      image: Ranchers_Garlic_Salami_200g,
+    },
+    {
+      name: 'Ranchers Roasted Turkey Fillet (200g)',
+      image: Ranchers_Roasted_Turkey_Fillet_200g,
+    },
+    { name: 'Ranchers Back Bacon (200g)', image: Ranchers_Back_Bacon_200g },
+    { name: 'Ranchers_Sandwich_Ham_200g', image: Ranchers_Sandwich_Ham_200g },
+    {
+      name: 'Ranchers Streaky Bacon (200g)',
+      image: Ranchers_Streaky_Bacon_200g,
     },
   ],
 };
