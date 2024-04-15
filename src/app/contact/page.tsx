1f64ae064660a0165faae9766f7376ca03ffd25f
George Omara <georgeomara8@gmail.com>
2024-04-15 13:39:58 +0300

    Include contact form description

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,8 @@
-import contact from '@/assets/hero/hero-contact.jpg';
+import contact from '@/assets/hero/hero-contact2.jpg';
 import ContactForm from '@/components/ui/form';
 import HeroSection from '@/components/ui/hero-section';
 import { Fragment } from 'react';
+import { proximaNova } from '../fonts';
 
 function Contact() {
   return (
@@ -10,6 +11,11 @@ function Contact() {
       <main className='w-full h-auto'>
         <div className='w-full px-8 py-20 md:px-10 md:py-28 xl:px-20  xl:container xl:mx-auto'>
           <div className='w-full lg:w-10/12 xl:w-8/12 mx-auto'>
+            <h2
+              className={`${proximaNova.className} font-light text-4xl text-center mb-8 `}
+            >
+              Feel free to send us a message!
+            </h2>
             <ContactForm />
           </div>
         </div>
