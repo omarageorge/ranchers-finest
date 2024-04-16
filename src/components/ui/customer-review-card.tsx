db7602c7167e798f2e297c5bc6a1531553062cc4
George Omara <georgeomara8@gmail.com>
2024-04-16 11:05:48 +0300

    Increase text height

--- src/components/ui/customer-review-card.tsx
+++ src/components/ui/customer-review-card.tsx
@@ -6,8 +6,8 @@ const CustomerReviewCard: React.FC<CustomerReviewCardProp> = ({
   message,
   customer,
 }) => (
-  <article className='w-full h-auto flex flex-col items-center justify-center gap-5 bg-whitish rounded-md px-4 py-6 xl:h-56'>
-    <p className='font-light text-lg text-center lg:text-xl'>{message}</p>
+  <article className='w-full h-auto flex flex-col items-center justify-center gap-y-2 bg-whitish rounded-md px-4 py-6 xl:h-64'>
+    <p className='font-light text-lg text-center xl:h-44'>{message}</p>
     <p className='font-bold text-xl text-center'>{customer}</p>
   </article>
 );
