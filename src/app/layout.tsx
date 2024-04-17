384e17491853f641aede7e263219f3aa77fdff84
George Omara <georgeomara8@gmail.com>
2024-04-17 09:25:03 +0300

    Add SEO Keywords

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,8 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'Ranchers Finest',
   description: "Kampala's Premier Sausages & Processed Meats",
+  keywords:
+    'sausages, beef sausages, pork sausages, chicken sausages, salami, ham, fresh meat, processed meat, cold cuts, frozen cuts, beef burgers, beef patties, Betar Sausages, steak, ribeye steak, rump steak, sirloin steak, pork chops, chicken legs, chicken thighs, chicken wings, chicken drumsticks, chicken fillet, beef mortadella, garlic salami, roasted turkey fillet, back bacon, sandwich ham, streaky bacon, Ugandan sausage company, Kampala meat company, Ranch Finest meats',
 };
 
 export default function RootLayout({
