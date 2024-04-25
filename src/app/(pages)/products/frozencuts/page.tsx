d31deb51531b61a4cfea47d682e983724cd254b5
George Omara <georgeomara8@gmail.com>
2024-04-25 11:20:38 +0300

    Display Betar Frozen Cuts

--- src/app/(pages)/products/frozencuts/page.tsx
+++ src/app/(pages)/products/frozencuts/page.tsx
@@ -3,6 +3,7 @@ import HeroSection from '@/components/ui/hero-section';
 import ProductGrid, {
   GridContainer,
 } from '@/components/ui/product/product-grid';
+import { betarFrozenCuts } from '@/lib/products/betar';
 import { frozenCuts } from '@/lib/products/frozen-cuts';
 import { Metadata } from 'next';
 import { Fragment } from 'react';
@@ -15,10 +16,16 @@ function FrozenCuts() {
   return (
     <Fragment>
       <HeroSection title='Frozen Cuts' image={heroMeat} />
-      <GridContainer padding>
+      <GridContainer>
         <ProductGrid
           products={frozenCuts.products}
           purchaseUrl={frozenCuts.purchaseUrl}
+          title='Premium Frozen Cuts'
+        />
+
+        <ProductGrid
+          products={betarFrozenCuts.products}
+          title='Betar Frozen Cuts'
         />
       </GridContainer>
     </Fragment>
