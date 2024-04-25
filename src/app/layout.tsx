37d7dcef333a8f9c6ee2fd6fdffc3713b57b570b
George Omara <georgeomara8@gmail.com>
2024-04-25 12:12:08 +0300

    Add metadataBase Url, opengraph title and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,23 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Ranchers Finest',
+  metadataBase: new URL('https://www.ranchersfinest.net'),
+  title: {
+    default: 'Home',
+    template: '%s - Ranchers Finest',
+  },
+  openGraph: {
+    title: 'Ranchers Finest',
+    description:
+      'The finest sausages, cold cuts, and fresh meats expertly crafted with passion in Kampala.',
+  },
   description:
     'Fresh, flavorful sausages, cold cuts, and meats crafted with passion. Experience exceptional quality in every bite. Browse our selection!',
   keywords:
     'kampala sausages, kampala fresh meat, kampala cold cuts, ugandan sausages, ugandan fresh meat, ugandan cold cuts, butcher kampala, meat shop kampala, deli kampala, beef sausages, betar sausages, breakfast beef sausages, traditional smoked beef sausages, pork sausages, breakfast pork sausages, bbq pork sausages, pork hotdogs, chicken sausages, english chicken sausages, breakfast chicken sausages, salami, garlic salami, ham, sandwich ham, back bacon, streaky bacon, beef mortadella, prime beef ribeye, prime beef rump steak, prime beef sirloin steak, prime beef steak, pork chops, pork cubes, beef burgers, beef burger patties, chicken legs, chicken thighs, chicken wings, chicken drumsticks, chicken fillet, ranchers finest sausages, ranchers finest meats, betar sausages uganda, where to buy beef sausages in kampala, best place for cold cuts in kampala, buy fresh chicken legs kampala, online butcher shop uganda, order steak online kampala',
+  twitter: {
+    card: 'summary_large_image',
+  },
 };
 
 export default function RootLayout({
