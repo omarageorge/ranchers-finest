3701f52f3d9b60e17d7503b1b9d8127369e14d9d
George Omara <georgeomara8@gmail.com>
2024-04-25 11:20:09 +0300

    Correct Product names

--- src/lib/products/betar.ts
+++ src/lib/products/betar.ts
@@ -1,6 +1,6 @@
 import Betar_Beef_Burgers_4pcs from '@/assets/products/betar/Betar_Beef_Burgers_4pcs.png';
 import Betar_Beef_Sausages_1kg from '@/assets/products/betar/Betar_Beef_Sausages_1kg.png';
-import Betar_Beefyz_Sausages from '@/assets/products/betar/Betar_Beefyz_Sausages.png';
+import Betar_Beefyz_Sausages_11pcs from '@/assets/products/betar/Betar_Beefyz_Sausages_11pcs.png';
 import Betar_Chicken_Sausages_1kg from '@/assets/products/betar/Betar_Chicken_Sausages_1kg.png';
 import Betar_Minced_Meat_1kg from '@/assets/products/betar/Betar_Minced_Meat_1kg.png';
 import Betar_Minced_Meat_500g from '@/assets/products/betar/Betar_Minced_Meat_500g.png';
@@ -8,16 +8,16 @@ import { ProductData } from '@/lib/definitions';
 
 export const betarSausages: ProductData = {
   products: [
-    { name: 'Beefyz Sausages', image: Betar_Beefyz_Sausages },
-    { name: 'Beef Sausages 1kg', image: Betar_Beef_Sausages_1kg },
-    { name: 'Chicken Sausages 1kg', image: Betar_Chicken_Sausages_1kg },
+    { name: 'Beefyz Sausages (11pcs)', image: Betar_Beefyz_Sausages_11pcs },
+    { name: 'Beef Sausages (1kg)', image: Betar_Beef_Sausages_1kg },
+    { name: 'Chicken Sausages (1kg)', image: Betar_Chicken_Sausages_1kg },
   ],
 };
 
-export const betarMeats: ProductData = {
+export const betarFrozenCuts: ProductData = {
   products: [
-    { name: 'Betar_Minced_Meat_500g', image: Betar_Minced_Meat_500g },
-    { name: 'Betar_Minced_Meat_1kg', image: Betar_Minced_Meat_1kg },
-    { name: 'Betar_Beef_Burgers_4pcs', image: Betar_Beef_Burgers_4pcs },
+    { name: 'Minced Meat (500g)', image: Betar_Minced_Meat_500g },
+    { name: 'Minced Meat (1kg)', image: Betar_Minced_Meat_1kg },
+    { name: 'Beef Burgers (4pcs)', image: Betar_Beef_Burgers_4pcs },
   ],
 };
