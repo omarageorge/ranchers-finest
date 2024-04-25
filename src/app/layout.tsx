08ef58f3af2aca1d7bf4009be6b1b9892c918b80
George Omara <georgeomara8@gmail.com>
2024-04-25 12:53:57 +0300

    Add opengraph title and description for twitter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ export const metadata: Metadata = {
   keywords:
     'kampala sausages, kampala fresh meat, kampala cold cuts, ugandan sausages, ugandan fresh meat, ugandan cold cuts, butcher kampala, meat shop kampala, deli kampala, beef sausages, betar sausages, breakfast beef sausages, traditional smoked beef sausages, pork sausages, breakfast pork sausages, bbq pork sausages, pork hotdogs, chicken sausages, english chicken sausages, breakfast chicken sausages, salami, garlic salami, ham, sandwich ham, back bacon, streaky bacon, beef mortadella, prime beef ribeye, prime beef rump steak, prime beef sirloin steak, prime beef steak, pork chops, pork cubes, beef burgers, beef burger patties, chicken legs, chicken thighs, chicken wings, chicken drumsticks, chicken fillet, ranchers finest sausages, ranchers finest meats, betar sausages uganda, where to buy beef sausages in kampala, best place for cold cuts in kampala, buy fresh chicken legs kampala, online butcher shop uganda, order steak online kampala',
   twitter: {
+    title: 'Ranchers Finest',
+    description:
+      'The finest sausages, cold cuts, and fresh meats expertly crafted with passion in Kampala.',
     card: 'summary_large_image',
   },
 };
