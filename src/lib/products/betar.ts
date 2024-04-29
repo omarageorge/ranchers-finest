4dc6c6f8fa190e5634db71c0cd02ec3f72af6bb5
George Omara <georgeomara8@gmail.com>
2024-04-29 15:57:32 +0300

    Add more Betar products to list

--- src/lib/products/betar.ts
+++ src/lib/products/betar.ts
@@ -1,16 +1,38 @@
 import Betar_Beef_Burgers_4pcs from '@/assets/products/betar/Betar_Beef_Burgers_4pcs.png';
+import Betar_Beef_Nyama_Nyama_Chilli_Sausages_20pcs from '@/assets/products/betar/Betar_Beef_Nyama_Nyama_Chilli_Sausages_20pcs.png';
+import Betar_Beef_Nyama_Nyama_Sausages_20pcs from '@/assets/products/betar/Betar_Beef_Nyama_Nyama_Sausages_20pcs.png';
 import Betar_Beef_Sausages_1kg from '@/assets/products/betar/Betar_Beef_Sausages_1kg.png';
+import Betar_Beef_Sausages_22pcs_1kg from '@/assets/products/betar/Betar_Beef_Sausages_22pcs_1kg.png';
 import Betar_Beefyz_Sausages_11pcs from '@/assets/products/betar/Betar_Beefyz_Sausages_11pcs.png';
 import Betar_Chicken_Sausages_1kg from '@/assets/products/betar/Betar_Chicken_Sausages_1kg.png';
+import Betar_Grilled_Beef_Sausages_22pcs_1kg from '@/assets/products/betar/Betar_Grilled_Beef_Sausages_22pcs_1kg.png';
+import Betar_Grilled_Chicken_Sausages_22pcs_1kg from '@/assets/products/betar/Betar_Grilled_Chicken_Sausages_22pcs_1kg.png';
 import Betar_Minced_Meat_1kg from '@/assets/products/betar/Betar_Minced_Meat_1kg.png';
 import Betar_Minced_Meat_500g from '@/assets/products/betar/Betar_Minced_Meat_500g.png';
 import { ProductData } from '@/lib/definitions';
 
 export const betarSausages: ProductData = {
   products: [
-    { name: 'Beefyz Sausages (11pcs)', image: Betar_Beefyz_Sausages_11pcs },
-    { name: 'Beef Sausages (1kg)', image: Betar_Beef_Sausages_1kg },
+    {
+      name: 'Beef Nyama Nyama Sausages (20pcs)',
+      image: Betar_Beef_Nyama_Nyama_Sausages_20pcs,
+    },
+    {
+      name: 'Beef Nyama Nyama Chilli Sausages (20pcs)',
+      image: Betar_Beef_Nyama_Nyama_Chilli_Sausages_20pcs,
+    },
     { name: 'Chicken Sausages (1kg)', image: Betar_Chicken_Sausages_1kg },
+    { name: 'Beefyz Sausages (11pcs)', image: Betar_Beefyz_Sausages_11pcs },
+    { name: 'Beef Sausages 22pcs (1kg)', image: Betar_Beef_Sausages_22pcs_1kg },
+    { name: 'Beef Sausages (1kg)', image: Betar_Beef_Sausages_1kg },
+    {
+      name: 'Grilled Chicken Sausages 22pcs (1kg)',
+      image: Betar_Grilled_Chicken_Sausages_22pcs_1kg,
+    },
+    {
+      name: 'Grilled Beef Sausages 22pcs (1kg)',
+      image: Betar_Grilled_Beef_Sausages_22pcs_1kg,
+    },
   ],
 };
 
