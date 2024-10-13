75171cb170fd28443f8899cbd7727aeb88f08739
George Omara <georgeomara8@gmail.com>
2024-10-13 17:27:23 +0300

    Create constant to store bacon images

---
+++ src/lib/bacon-images.tsx
@@ -0,0 +1,9 @@
+import image1 from '@/assets/bacon/slides/01.jpg';
+import image2 from '@/assets/bacon/slides/02.jpg';
+import image3 from '@/assets/bacon/slides/03.jpg';
+import image4 from '@/assets/bacon/slides/04.jpg';
+import { StaticImageData } from 'next/image';
+
+const baconImages: StaticImageData[] = [image1, image2, image3, image4];
+
+export default baconImages;
