242d58657b09a8704400e23196677983c92134df
George Omara <georgeomara8@gmail.com>
2024-10-13 19:54:37 +0300

    Organize permits and certificate images

---
+++ src/app/(pages)/certificates/images.ts
@@ -0,0 +1,18 @@
+import kcca from '@/assets/ranchers-certs/kcca.jpg';
+import halaal from '@/assets/ranchers-certs/halaal.jpg';
+import iso from '@/assets/ranchers-certs/iso.jpg';
+import beefCutsPermit from '@/assets/ranchers-certs/beef-cuts-permit.jpg';
+import mincedBeefPermit from '@/assets/ranchers-certs/minced-beef-permit.jpg';
+import goatCutsPermit from '@/assets/ranchers-certs/goat-cuts-permit.jpg';
+import lambPermit from '@/assets/ranchers-certs/lamb-permit.jpg';
+
+export const essentialCertificates = [kcca, halaal];
+
+export const unbsPermits = [
+  beefCutsPermit,
+  mincedBeefPermit,
+  goatCutsPermit,
+  lambPermit,
+];
+
+export const otherCertificates = [iso];
